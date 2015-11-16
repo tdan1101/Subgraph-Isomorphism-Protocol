c4c99a990706ca6cceeb6a277d9d32b906b5ec0b
XJi <xiaojingji5@gmail.com>
2015-11-16 13:19:58 -0500

    Add functions to Prover and Verifier

---
+++ SIProtocol/src/Prover.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.net.Socket;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import HelperClass.FileReader;
+
+/**
+ * 
+ */
+public class Prover {
+	 private static Socket socket;
+	 static int[][] matrix;
+	 
+	 /**
+	  * Convert an adjacency matrix to string before sending
+	  * it to the verifier
+	  * @param matrix
+	  * @return string
+	  */
+	 public static String convertToString(int[][] matrix){
+		 String buffer = "";
+		 int length = matrix[0].length;
+		 for(int i = 0; i < length; i++){
+			 for(int j = 0; j < length; j++)
+				 buffer += matrix[i][j];
+		 }
+		 return buffer;
+	 }
+	 
+	 
+	 
+	 public static void main(String args[]){
+	     try {
+	         String host = "localhost";
+	         /* Get the server address from a dialog box.
+	            If prover and verifier are running on the 
+	            same iOS machine, leave IPAddress empty*/
+	         String serverAddress = JOptionPane.showInputDialog(
+	                    "Enter IP Address of a machine that is\n" +
+	                    "If it's running on iOS terminal, leave it empty");
+	         socket = new Socket(serverAddress, 6077);
+	         //Send the message to the server
+	         OutputStream os = socket.getOutputStream();
+	         OutputStreamWriter osw = new OutputStreamWriter(os);
+	         BufferedWriter bw = new BufferedWriter(osw);
+	        /* JFrame f = new JFrame();
+	         JFileChooser fc = new JFileChooser();
+	         int ret = fc.showOpenDialog(f);
+			 if(ret==JFileChooser.APPROVE_OPTION){
+				 File file = fc.getSelectedFile();
+				 String name = file.getName();
+				 System.out.println(name);
+				 matrix = FileReader.readGraph(name);
+				 f.setVisible(true);
+				 
+			 }*/
+	         matrix = FileReader.readGraph("/g1");
+	         String buffer = convertToString(matrix);
+	         String sendMessage = buffer + "\n";
+	         bw.write(sendMessage);
+	         bw.flush();
+	         System.out.println("Message sent to the server : "+sendMessage);
+	         //Get the return message from the server
+	         InputStream is = socket.getInputStream();
+	         InputStreamReader isr = new InputStreamReader(is);
+	         BufferedReader br = new BufferedReader(isr);
+	         String message = br.readLine();
+	         System.out.println("Message received from the server : " +message);
+	      }
+	      catch (Exception exception){
+	          exception.printStackTrace();
+	      }
+	      finally{
+	          //Closing the socket
+	          try{
+	                socket.close();
+	            }
+	          catch(Exception e){
+	                e.printStackTrace();
+	          }
+	     }
+	}
+}
---
+++ SIProtocol/src/Verifier.java
@@ -0,0 +1,85 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * In Subgraph Isomorphism Protocol, verifier runs Server 
+ * to do the following:
+ * 	1. Receive and accept commitment(Q), where Q = a(G2)
+ *  2. Send a random bit to the Prover(Client)
+ *  	0: request permutation a
+ *      1: request pi and Q' such that Q' = pi(G1)
+ *  3. 	Verify the data received from Prover
+ */
+public class Verifier {
+	private static Socket socket;
+	private static int SERVERPORT = 6077;
+	
+	/**
+	 * Receive commitment(Q) from the prover
+	 * @param Socket
+	 * @throws IOException
+	 * @return String of the data received from prover 
+	 */
+	public static String receiveBuffer(Socket socket){
+		try{
+			InputStream is = socket.getInputStream();
+	        InputStreamReader isr = new InputStreamReader(is);
+	        BufferedReader br = new BufferedReader(isr);
+	        String buffer = br.readLine();
+	        System.out.println("Message received from client is "+ buffer);
+	        return buffer;
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	/**
+	 * Convert the received buffer to an adjacency matrix
+	 * @param buffer
+	 * @return 2D adjacency matrix
+	 */
+	public static int[][] convertToMatrix(String buffer){	
+		int size = (int)Math.sqrt(buffer.length());
+		int[][] adjMatrix = new int[size][size];
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j < size; j++)
+				adjMatrix[i][j] = (int)buffer.charAt(i*size+j);
+		}
+		return adjMatrix;
+	}
+	
+	
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(SERVERPORT);
+        try {
+            while (true) {
+            	socket = serverSocket.accept();
+            	System.out.println("Accept.\n"
+            			+ "Request the commitment of Q");
+            	/* Receive commitment(Q) */
+                String s = receiveBuffer(socket);
+                // Get commitment Q from the prover
+                int[][] commitmentQ = convertToMatrix(s);
+                int bit = (int)(Math.random()+0.5);
+                OutputStream os = socket.getOutputStream();
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+   	         	BufferedWriter bw = new BufferedWriter(osw);
+   	         	bw.write(bit);
+   	         	bw.flush();
+                
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally {
+            socket.close();
+        }
+    }
+}
